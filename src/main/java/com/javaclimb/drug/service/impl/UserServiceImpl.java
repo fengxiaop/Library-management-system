6ed1362a6c480912d578cffaee20b0868f710e94
“xiaopeng” <19408000322@stu.hut.edu.cn>
2021-12-09 11:05:40 +0800

    upload user.html

--- src/main/java/com/javaclimb/drug/service/impl/UserServiceImpl.java
+++ src/main/java/com/javaclimb/drug/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.javaclimb.drug.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.javaclimb.drug.entity.User;
 import com.javaclimb.drug.mapper.UserMapper;
@@ -22,32 +24,37 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
     @Override
     public IPage<User> selectUserPage(int pageNum, int pageSize, String param) {
-        return null;
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        if(StringUtils.isNotEmpty(param)){
+            queryWrapper.like("username",param);
+        }
+        Page<User> page = new Page<>(pageNum,pageSize);
+        return userMapper.selectPage(page,queryWrapper);
     }
 
     @Override
     public int addUser(User user) {
-        return 0;
+        return userMapper.insert(user);
     }
 
     @Override
     public int editUser(User user) {
-        return 0;
+        return userMapper.updateById(user);
     }
 
     @Override
     public User queryUserById(Integer id) {
-        return null;
+        return userMapper.selectById(id);
     }
 
     @Override
     public int delUserById(Integer id) {
-        return 0;
+        return userMapper.deleteById(id);
     }
 
     @Override
     public List<User> queryUserList() {
-        return null;
+        return userMapper.selectList(null);
     }
 
     /**
