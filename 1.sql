44daeeecbdf4b1b629f2092ec74375fb9f3a5df3
pengjin <1493020035@qq.com>
2023-10-05 09:55:24 +0800

    FIX: 修复数据库连接不上问题

--- 1.sql
+++ 1.sql
@@ -1,8 +1,9 @@
 /*
 SQLyog Ultimate v10.00 Beta1
-MySQL - 5.7.36 : Database - drug
+MySQL - 5.7.36 : Database - book
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +13,9 @@ MySQL - 5.7.36 : Database - drug
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`drug` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`book` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `drug`;
+USE `book`;
 
 /*Table structure for table `druginfo` */
 
